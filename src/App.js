43b1e8957ef63b8606bf628bd7cde265bdd6536f
rishav-jha-mech <rishav.rj74@gmail.com>
2023-01-21 22:36:21 +0530

    Removed useless files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <h1>
+        <ol>
+          <li>Install bootstrap</li>
+          <li>Add Hero section</li>
+          <li>Add detail section</li>
+          <li>Add registration form</li>
+        </ol>
+      </h1>
     </div>
   );
 }
