024188763df84642a8482d283190454f6aaa061a
Gayatri Agarwal <gayatri19agarwal@gmail.com>
2023-01-22 21:14:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,148 +1,166 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
-import hero from './assets/images/hero.jpg';
-import human from './assets/images/human.png';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Card from 'react-bootstrap/Card';
+import cca from './assets/images/ccalogo1.png';
+import wdct from './assets/images/wdct1.png';
+import wdctCorner from './assets/images/WDCTCorner.png';
+import interview from './assets/images/undraw_interview.png';
+import certificate from './assets/images/Certificate.png';
 
 
 function App() {
   return (
     <>
-    <Navbar className='nav' collapseOnSelect expand="lg" >
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#pricing">Details</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div>
+    <div id='home'>
       <div className='hero'>
-        <img src={hero} style={ { width: '100%'} }/>
+        <div className='heroText'>
+          <h1 textAlign="center" align="center">Design Workshop</h1>
+          <h3 className='my-4'> Learn Adobe Illustrator from scratch in two days</h3>
+        </div>
+        <div>
+          <Button className='px-5 py-3 mt-4 mx-2' size="md" variant="outline-info">Learn More</Button>{' '}
+          <Button className='px-5 py-3 mt-4 mx-2' size="md" variant="outline-info">Register</Button>{' '}
+        </div>
       </div>
     </div>
-    <div className='cards'>
-      <h2 className='cardHeading' align="center">TIMELINE</h2>
+    <div className='container'>
+      <div className='description'>
+        <h2>Description</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis culpa reprehenderit veritatis repudiandae, praesentium quae autem nihil non suscipit voluptatum explicabo, quasi officia at. Numquam nisi aspernatur quas libero ducimus?</p>
+      </div>
+      <div className='details'>
+        <div className='day'>
+            <p>Day1: </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis culpa reprehenderit veritatis repudiandae, praesentium quae autem nihil non suscipit voluptatum explicabo, quasi officia at. Numquam nisi aspernatur quas libero ducimus?</p>
+        </div>
+        <div className='day'>
+            <p>Day2: </p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis culpa reprehenderit veritatis repudiandae, praesentium quae autem nihil non suscipit voluptatum explicabo, quasi officia at. Numquam nisi aspernatur quas libero ducimus?</p>
+        </div>
+      </div>
+      <div className='description'>
+        <h2>What’s in for you</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis culpa reprehenderit veritatis repudiandae, praesentium quae autem nihil non suscipit voluptatum explicabo, quasi officia at. Numquam nisi aspernatur quas libero ducimus?</p>
+      </div>
       <Row>
-        <Col sm={3}></Col>
-        <Col align="center" sm={3}>
-        <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+        <Col sm={4}>
+        <Card className='card' style={{ width: '18rem' }}>
+          <Card.Img variant="top" className='certificate' src= {certificate} style={{ width: '50%' }}/>
+          <Card.Body>
+            <Card.Title align='center'>Certificate</Card.Title>
+            <Card.Text align='center'>
+              of participation for all the 
+              participants
+            </Card.Text>
+          </Card.Body>
+        </Card>
         </Col>
-        <Col align="center" sm={3}>
-        <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+        <Col sm={4}>
+        <Card className='card' style={{ width: '18rem' }}>
+          <Card.Img className='wdctCorner' variant="top" src={wdctCorner} />
+          <Card.Body>
+            <Card.Title align='center'>Top Entries</Card.Title>
+            <Card.Text align='center'>
+            will be featured in 
+            WDCT corner.
+            </Card.Text>
+          </Card.Body>
+        </Card>
         </Col>
-        <Col sm={3}></Col>
-      </Row>
-    </div>
-    <div className='register'>
-      <Row>
-        <Col sm={6} className= 'Regimage'> <img src={human} /> </Col>
-        <Col sm={6}>
-        <Form className='p-5'>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Name" />
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="tel" placeholder="Phone Number " />
-            </Form.Group>
-          </Row>
-
-          <Form.Group className="mb-3" controlId="formGridAddress1">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type='email' placeholder="Email" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>College Name</Form.Label>
-            <InputGroup className="mb-3">
-            <Form.Control aria-label="Text input with dropdown button" placeholder="College Name" />
-
-            <DropdownButton
-              variant="outline-secondary"
-              title=""
-              id="input-group-dropdown-2"
-              align="end"
-            >
-              <Dropdown.Item href="#">NIT Durgapur</Dropdown.Item>
-            </DropdownButton>
-            </InputGroup>
-          </Form.Group>
-
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridCity">
-              <Form.Label>Department</Form.Label>
-              <Form.Control placeholder="Department"/>
-            </Form.Group>
-
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>Year</Form.Label>
-              <Form.Select defaultValue="!st Year">
-                <option>1st Year</option>
-                <option>2nd Year</option>
-                <option>3rd Year</option>
-                <option>4th Year</option>
-              </Form.Select>
-            </Form.Group>
-          </Row>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+        <Col sm={4}>
+        <Card className='card' style={{ width: '18rem' }}>
+          <Card.Img variant="top" className='interview' src={interview} style={{ width: '80%' }}/>
+          <Card.Body>
+            <Card.Title align='center'>Top Performers</Card.Title>
+            <Card.Text align='center'>
+            will get an advantage in
+            auditions
+            </Card.Text>
+          </Card.Body>
+        </Card>
         </Col>
       </Row>
+      <div className='description'>
+        <h2>Mentor</h2>
+      </div>
+      <div className='mentor'>
+        <div>
+          <div>
+            <img></img>
+          </div>
+          <div>
+            <h3>Sayan Majumder</h3>
+            <h4>Head, WDCT,CCA</h4>
+            <h3>Be In</h3>
+          </div>
+        </div>
+        <div>
+          <p>
+          Every firm has stories to tell—stories that will not only engage, inform, surprise, delight, and impact their audience, but that will also deliver on measurable goals. And I am the conduit between the firm and consumer.
+          </p>
+          <p>
+            I help clients find the subject and medium that best fits their unique identity, and then I produce high-quality content that meets their objectives.
+          </p>
+        </div>
+      </div>
+      <div className='description'>
+        <h2>Coordinators</h2>
+      </div>
+      <Row>
+        <Col sm={3}>
+        <Card className='coordinatorCard' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title align='center'>Somwrik Dubey</Card.Title>
+            <Card.Text align='center'>
+            Senior member
+            WDCT,CCA
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col sm={3}>
+        <Card className='coordinatorCard' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title align='center'>Swarnalim Sonowal</Card.Title>
+            <Card.Text align='center'>
+            Senior member
+            WDCT,CCA
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col sm={3}>
+        <Card className='coordinatorCard' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title align='center'>Neeldari Dhar</Card.Title>
+            <Card.Text align='center'>
+            Senior member
+            WDCT,CCA
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </Col>
+        <Col sm={3}>
+        <Card className='coordinatorCard' style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title align='center'>Saikat Sarkar</Card.Title>
+            <Card.Text align='center'>
+            Senior member
+            WDCT,CCA
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        </Col>
+      </Row>
     </div>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <h4>Footer</h4>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
     </>
   );
 }
